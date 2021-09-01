85373999729f0a6a8ebdd041a4356ce6df4ec7e9
James <jamesryu108@gmail.com>
2021-09-01 14:27:56 -0400

    Initial Commit

---
+++ freelance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  freelance
+//
+//  Created by James Ryu on 2021-09-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
